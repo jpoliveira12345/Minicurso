74b2fec965121c43ecb3c62cd3cebfc9f7ff6bee
João Paulo <joaopaulodeoliveira123@gmail.com>
2018-05-21 20:11:16 -0300

    criação da calculadora

---
+++ calculadora.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//menu
+//soma
+//subtração
+//raiz quadrada
+//eleva ao quadrado
+//eleva a x
+//divisão
+//multiplicação
+//porcentagem
+//retorna pi
+//inverso (1/x)
+//fatorial
+//retorna exp
+int main(){
+
+
+}
