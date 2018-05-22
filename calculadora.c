cdb69691b98825201ffd44fbbde9de4e6f5408fe
Leonardo Ferreira <leoessia1@outlook.com>
2018-05-22 17:01:06 -0300

    muitos

--- calculadora.c
+++ calculadora.c
@@ -9,11 +9,35 @@ int sum(int a, int b)
 	return a + b;
 }
 //subtração
+int sub(int a, int b)
+{
+	return a - b;
+}
 //raiz quadrada
+int sqqrt(int a)
+{
+	return sqrt(a);
+}
 //eleva ao quadrado
+int ppow(int a)
+{
+	return pow(a, 2);
+}
 //eleva a x
+int eleva(int a, int x)
+{
+	return pow(a, x);
+}
 //divisão
+int div(int a, int b)
+{
+	return a / b;
+}
 //multiplicação
+int mult(int a, int b)
+{
+	return a * b;
+}
 //porcentagem
 //retorna pi
 //inverso (1/x)
