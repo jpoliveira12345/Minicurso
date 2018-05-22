61ee28cf01bc8c62af239e34d9d312e066ad3f73
Sekktion <viniciusmeres1@gmail.com>
2018-05-22 17:26:22 -0300

    função fatorial

--- calculadora.c
+++ calculadora.c
@@ -13,6 +13,17 @@
 //retorna pi
 //inverso (1/x)
 //fatorial
+int i, x, total, y;
+	printf("Insira um numero 'x'\n");
+	scanf("%d",&x);
+	i=0;
+	total=1;
+	while(i<x)
+	{
+		total=total*(x-i);
+		i++;
+	}
+	printf("total: %d\n",total);
 //retorna exp
 //seno
 //cosseno
