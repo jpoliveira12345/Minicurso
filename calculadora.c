18b8e46656812a1c889ca59541d3697e54fccce6
Rodrigo Zamboni <rodrigo-zamboni@hotmail.com>
2018-05-22 17:01:40 -0300

    funcoespracalc

--- calculadora.c
+++ calculadora.c
@@ -3,14 +3,27 @@
 
 //menu
 //soma
+int soma(int a,int b){
+return a+b;
+}
 //subtração
+int subtração(int a, int b){
+	return a-b;
+}
 //raiz quadrada
 //eleva ao quadrado
+int pow(int a){
+	return a*a;
+}
 //eleva a x
 //divisão
 //multiplicação
 //porcentagem
 //retorna pi
+float retorna_pi{
+return 3.14159
+
+}
 //inverso (1/x)
 //fatorial
 //retorna exp
