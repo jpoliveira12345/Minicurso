98a06ce9c0164b545bb4ce11b0cf38bdcf999c41
thaynarasilvapinto <thay.silva.ptc@gmail.com>
2018-05-22 16:57:40 -0300

    Fibonacci feitogit add . --- Joaooo feio

--- calculadora.c
+++ calculadora.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,6 +19,18 @@
 //cosseno
 //tangent
 //fibonacci
+int fibonacci(int n){
+    if(n==0){
+        return 0;
+    }else{
+        if(n==1){
+            return 1;
+        }
+        else{
+            return (fibonacci(n-1)+fibonacci(n-2));
+        }
+    }
+}
 //média de n elementos
 int main(){
 
